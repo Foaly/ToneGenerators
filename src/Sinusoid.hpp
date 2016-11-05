9e88586df0bcbe1b84bd809505e4235faea22887
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2016-11-05 15:47:33 +0100

    Optimized the sine wave generator for constant frequencies using matrix rotation.

--- src/Sinusoid.hpp
+++ src/Sinusoid.hpp
@@ -3,29 +3,42 @@
 
 #include <cmath>
 
+/*
+ * \brief A sine wave generator. It uses matrix rotation as an optimization.
+ *        See https://en.wikipedia.org/wiki/Rotation_matrix#In_two_dimensions
+ *        Note this implementation is optimized for sinusoids whos frequency stays the
+ *        same between calls to getNextSample(). If the frequency is changed between 
+ *        every call to getNextSample() a naive implementation using std::sin() will be
+ *        more efficent. Also note that the phase stays continous when the frequency changes.
+ */
+
 class Sinusoid
 {
 public:
     Sinusoid(double _frequency, double _amplitude, double _sampleRate) :
         amplitude(_amplitude),
         sampleRate(_sampleRate),
-        phi(0.0)
+        m_x(0.0),
+        m_y(1.0)
     {
         frequency(_frequency);
     }
 
     double getNextSample()
     {
-        double sample = std::sin(phi);
-
-        phi += twoPI * m_frequency / sampleRate;
-
-        return amplitude * sample;
+        double oldX = m_x;
+        m_x = m_x * m_cosinus + m_y * m_sinus;
+        m_y = oldX * -m_sinus + m_y * m_cosinus;
+        
+        return amplitude * oldX;
     }
     
     void frequency(double frequency)
     {
         m_frequency = frequency;
+        double step = twoPI * m_frequency / sampleRate;
+        m_sinus = std::sin(step);
+        m_cosinus = std::cos(step);
     }
     
     double frequency()
@@ -35,11 +48,13 @@ public:
 
     double amplitude;
     double sampleRate;
-    double phi;
     
 private:
     double m_frequency;
-
+    double m_sinus;
+    double m_cosinus;
+    double m_x;
+    double m_y;
 };
 
 #endif // SINUSOID_INCLUDE
